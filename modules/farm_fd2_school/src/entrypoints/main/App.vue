ecd1b48b2b7867649f580006aa9519851b4f9575
braught <braught@dickinson.edu>
2023-07-21 09:20:29 -0400

    Adding school module

---
+++ modules/farm_fd2_school/src/entrypoints/main/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    The FarmData2 School module appears in the development environment. It provides a working space
+    for the FD2School Onboarding activities. Each of the activities create a new entry point in this
+    menu as a way to learn about developing FarmData2 features.
+  </div>
+  <div data-cy="page-loaded" v-show="false">{{ pageDoneLoading }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      createdCount: 0,
+    }
+  },
+  computed: {
+    pageDoneLoading() {
+      return this.createdCount == 1
+    },
+  },
+  created() {
+    this.createdCount++
+  },
+}
+</script>
---
+++ modules/farm_fd2_school/src/module/Controller/FD2_Controller.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\farm_fd2_school\Controller\FD2_Controller.
+ */
+namespace Drupal\farm_fd2_school\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class FD2_Controller extends ControllerBase
+{
+  public function content()
+  {
+    // Get the name of the FarmData endpoint being requested from the URL
+    // e.g. main or FieldKit or ...
+    $current_path = \Drupal::service('path.current')->getPath();
+    $fd2_service = substr($current_path, 14);
+
+    return [
+      'app' => [
+        '#markup' => '<div id="app"></div>',
+        '#attached' => [
+          'library' => ['farm_fd2_school/' . $fd2_service],
+        ],
+      ],
+    ];
+  }
+}
