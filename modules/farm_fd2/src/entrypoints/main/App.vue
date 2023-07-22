441fe86c4229d0599b326eab22913ffb7440addd
braught <braught@dickinson.edu>
2023-07-22 18:13:57 -0400

    test formatting

--- modules/farm_fd2/src/entrypoints/main/App.vue
+++ modules/farm_fd2/src/entrypoints/main/App.vue
@@ -2,8 +2,8 @@
   <div>
     FarmData2 provide entry and reporting features supporting:
     <ul>
-        <li>the day-to-day operation of diversified vegetable farms.</li>
-      <li>the organic certification process.</li>
+      <li>the day-to-day operation of diversified vegetable farms.</li>
+        <li>the organic certification process.</li>
       <li>sustainable farming practices including soil health.</li>
     </ul>
   </div>
