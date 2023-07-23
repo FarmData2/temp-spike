2411dc4914d90792fc66e37840c3ccc14e654059
braught <braught@dickinson.edu>
2023-07-23 13:49:29 -0400

    test examples vue change

--- modules/farm_fd2/src/entrypoints/main/App.vue
+++ modules/farm_fd2/src/entrypoints/main/App.vue
@@ -5,7 +5,6 @@
       <li>the day-to-day operation of diversified vegetable farms.</li>
       <li>the organic certification process.</li>
       <li>sustainable farming practices including soil health.</li>
-      <li>blah</li>
     </ul>
   </div>
   <div data-cy="page-loaded" v-show="false">{{ pageDoneLoading }}</div>
