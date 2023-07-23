dfa8473d968866969325f180f6c9b712aed2b056
braught <braught@dickinson.edu>
2023-07-23 13:53:28 -0400

    PR Preflight done

--- modules/farm_fd2_examples/src/entrypoints/main/App.vue
+++ modules/farm_fd2_examples/src/entrypoints/main/App.vue
@@ -2,7 +2,6 @@
   <div>
     The FarmData2 Examples module appears in the development environment. It provides canonical
     examples of how FarmData2 entry points and components are created, used, and tested.
-    
   </div>
   <div data-cy="page-loaded" v-show="false">{{ pageDoneLoading }}</div>
 </template>
